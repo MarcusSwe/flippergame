43ec36aa1e4d4cd4a64ab697fc9996ea0036e706
marcus.larsen <marcus.larsen.swe@gmail.com>
2022-09-08 23:18:03 +0200

    test

---
+++ backend/src/main/java/com/example/flipperbackend/FlipperBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.flipperbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlipperBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlipperBackendApplication.class, args);
+    }
+
+}
