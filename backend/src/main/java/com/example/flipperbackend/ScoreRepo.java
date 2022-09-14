99f1c5d290ebaffeac784a52f6a793411cb63881
marcus.larsen <marcus.larsen.swe@gmail.com>
2022-09-15 00:54:41 +0200

    lol

--- backend/src/main/java/com/example/flipperbackend/ScoreRepo.java
+++ backend/src/main/java/com/example/flipperbackend/ScoreRepo.java
@@ -1,15 +1,8 @@
 package com.example.flipperbackend;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
-
 import java.util.List;
 
 public interface ScoreRepo extends MongoRepository<Score, Long> {
-
-    //@Query(fields="{'name' : 1, 'score' : 1}")
     List<Score> findAll();
-
-    // public long count();
-
 }
