5b9f28fac0e23c3eaff6fbe6d34c6705c3bf7dbe
marcus.larsen <marcus.larsen.swe@gmail.com>
2022-09-15 00:48:53 +0200

    lol

--- backend/src/main/java/com/example/flipperbackend/ServiceFlipper.java
+++ backend/src/main/java/com/example/flipperbackend/ServiceFlipper.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ServiceFlipper {
@@ -17,7 +18,8 @@ public class ServiceFlipper {
     }
 
     public List<ScoreDTO> getScores() {
-        return null;
+        List<Score> scores = scoreRepo.findAll();
+        return scores.stream().map(x -> new ScoreDTO(x.getName(), x.getScore())).collect(Collectors.toList());
     }
 
     public String addScore(Score score){
